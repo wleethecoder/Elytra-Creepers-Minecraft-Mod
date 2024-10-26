610d4d632b582935282876df9ca868d40aac554b
Wenhan-Samuel-Lee <wenhan_lee@utexas.edu>
2024-10-26 09:40:43 -0500

    once again, can confirm that non-player entities can fly with an elytra

---
+++ src/main/java/com/leecrafts/elytracreepers/event/ModEvents.java
@@ -0,0 +1,47 @@
+package com.leecrafts.elytracreepers.event;
+
+import com.leecrafts.elytracreepers.ElytraCreepers;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.monster.Creeper;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.neoforge.event.tick.EntityTickEvent;
+
+@EventBusSubscriber(modid = ElytraCreepers.MODID, bus = EventBusSubscriber.Bus.GAME)
+public class ModEvents {
+
+    @SubscribeEvent
+    public static void putElytraTest(PlayerInteractEvent.EntityInteract event) {
+        Entity target = event.getTarget();
+        if (!target.level().isClientSide && target instanceof LivingEntity creeper) {
+            creeper.setItemSlot(EquipmentSlot.CHEST, new ItemStack(Items.ELYTRA));
+            System.out.println(creeper.getItemBySlot(EquipmentSlot.CHEST).getItem());
+        }
+    }
+
+    @SubscribeEvent
+    public static void fallFlyingTest(EntityTickEvent.Pre event) {
+        Entity entity = event.getEntity();
+        if (!entity.level().isClientSide &&
+                entity instanceof Creeper creeper &&
+                !creeper.onGround() &&
+                !creeper.isFallFlying()) {
+            CompoundTag compoundTag = creeper.saveWithoutId(new CompoundTag());
+            compoundTag.putBoolean("FallFlying", true);
+            creeper.load(compoundTag);
+//            creeper.startFallFlying();
+//            ItemStack itemstack = this.getItemBySlot(EquipmentSlot.CHEST);
+//            if (itemstack.canElytraFly(this)) {
+//                this.startFallFlying();
+//                return true;
+//            }
+        }
+    }
+
+}
