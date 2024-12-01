f2b986326c7c6ff2e1ffef3f976f434986e013a2
Wenhan-Samuel-Lee <wenhan_lee@utexas.edu>
2024-12-01 16:16:16 -0600

    displaying genome of agent from the training process

--- src/main/java/com/leecrafts/elytracreepers/neat/util/NEATTest.java
+++ src/main/java/com/leecrafts/elytracreepers/neat/util/NEATTest.java
@@ -10,8 +10,8 @@ import java.io.*;
 public class NEATTest {
 
     private static final double[] in = new double[] {0.04406537922176701, 0.18126992327777725, 0.24199198935168187, 0.1813263608295541, 0.24448311455523108, 0.14145820074195736, 0.2602359917148832, 0.3497226936060528, 0.9711452639458819, 0.8405658596218063};
-    private static final String AGENT_DIRECTORY_PATH = "/assets/elytracreepers/agent";
-    public static final File MODEL_DIRECTORY_PATH = new File(System.getProperty("user.dir"), "src/main/resources" + AGENT_DIRECTORY_PATH);
+    private static final String ASSETS_DIRECTORY_PATH = "/assets/elytracreepers/agent";
+    public static final File AGENT_DIRECTORY_PATH = new File(System.getProperty("user.dir"), "src/main/resources" + ASSETS_DIRECTORY_PATH);
 
     private static void emptyGenomeSize() {
         NEATController neatController = new NEATController(4,10,50);
@@ -53,7 +53,7 @@ public class NEATTest {
         Agent agent = neatController.getBestAgent();
 
         // saving
-        File file = new File(MODEL_DIRECTORY_PATH, "agent.dat");
+        File file = new File(AGENT_DIRECTORY_PATH, "agent.dat");
         try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(file))) {
             out.writeObject(agent);
             System.out.println("Agent saved to " + file.getPath());
@@ -76,11 +76,23 @@ public class NEATTest {
         }
     }
 
+    private static void printGenomeOfEntityAgent(int agentNumber) {
+        File file = new File(AGENT_DIRECTORY_PATH, String.format("%s-%d.dat", "agent", agentNumber));
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(file))) {
+            Agent agent = (Agent) in.readObject();
+            System.out.println("Agent loaded from " + file.getPath());
+            new Frame(agent.getGenome());
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Error loading agent: " + e.getMessage());
+        }
+    }
+
     public static void main(String[] args) {
 //        emptyGenomeSize();
 //        displayFrame();
 //        agents(true);
-        saveAndLoad();
+//        saveAndLoad();
+        printGenomeOfEntityAgent(2);
     }
 
 }
