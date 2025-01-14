f89f64fa6b4f0a22fc83a09554e3c3837d786087
Wenhan-Samuel-Lee <wenhan_lee@utexas.edu>
2025-01-13 23:43:56 -0600

    got rid of neoforge provided configs

--- src/main/java/com/leecrafts/elytracreepers/Config.java
+++ src/main/java/com/leecrafts/elytracreepers/Config.java
@@ -21,37 +21,20 @@ public class Config
 {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
-    private static final ModConfigSpec.BooleanValue LOG_DIRT_BLOCK = BUILDER
-            .comment("Whether to log the dirt block on common setup")
-            .define("logDirtBlock", true);
-
-    private static final ModConfigSpec.IntValue MAGIC_NUMBER = BUILDER
-            .comment("A magic number")
-            .defineInRange("magicNumber", 42, 0, Integer.MAX_VALUE);
-
-    public static final ModConfigSpec.ConfigValue<String> MAGIC_NUMBER_INTRODUCTION = BUILDER
-            .comment("What you want the introduction message to be for the magic number")
-            .define("magicNumberIntroduction", "The magic number is... ");
-
-    // a list of strings that are treated as resource locations for items
-    private static final ModConfigSpec.ConfigValue<List<? extends String>> ITEM_STRINGS = BUILDER
-            .comment("A list of items to log on common setup.")
-            .defineListAllowEmpty("items", List.of("minecraft:iron_ingot"), Config::validateItemName);
-
     private static final ModConfigSpec.ConfigValue<String> SPAWNED_ENTITY_TYPE = BUILDER
             .comment("The type of entities that will spawn with a neural elytra")
             .define("spawned_entity_type", "minecraft:creeper");
 
-    private static final ModConfigSpec.ConfigValue<Boolean> GRIEFING = BUILDER
+    private static final ModConfigSpec.BooleanValue GRIEFING = BUILDER
             .comment("Whether or not entities flying with a neural elytra are able to destroy blocks (e.g. via explosions)")
             .define("griefing", false);
 
-    private static final ModConfigSpec.ConfigValue<Boolean> AUTO_IGNITE = BUILDER
+    private static final ModConfigSpec.BooleanValue AUTO_IGNITE = BUILDER
             .comment("Whether or not creepers flying with a neural elytra ignite automatically when landing on the ground " +
                     "(it also has to be within 7 blocks of its target)")
             .define("auto_ignite", true);
 
-    private static final ModConfigSpec.ConfigValue<Boolean> EXPLODE_HURT_ONLY_TARGET = BUILDER
+    private static final ModConfigSpec.BooleanValue EXPLODE_HURT_ONLY_TARGET = BUILDER
             .comment("Whether or not creepers flying with a neural elytra hurt only the target when exploding")
             .define("explode_hurt_only_target", true);
 
@@ -63,19 +46,19 @@ public class Config
             .comment("Length of time interval (in seconds) between spawns")
             .defineInRange("spawn_interval", 60, 5, 180);
 
-    private static final ModConfigSpec.ConfigValue<Boolean> NIGHT_ONLY_SPAWN = BUILDER
+    private static final ModConfigSpec.BooleanValue NIGHT_ONLY_SPAWN = BUILDER
             .comment("Whether or not entities with a neural elytra spawn only in the night")
             .define("night_only_spawn", true);
 
-    private static final ModConfigSpec.ConfigValue<Boolean> INSOMNIA_ONLY_SPAWN = BUILDER
+    private static final ModConfigSpec.BooleanValue INSOMNIA_ONLY_SPAWN = BUILDER
             .comment("Whether or not entities with a neural elytra spawn only when the player has insomnia")
             .define("insomnia_only_spawn", false);
 
-    private static final ModConfigSpec.ConfigValue<Boolean> SUBTITLE_WARN = BUILDER
+    private static final ModConfigSpec.BooleanValue SUBTITLE_WARN = BUILDER
             .comment("Displays a subtitle whenever entities with a neural elytra spawn")
             .define("subtitle_warn", true);
 
-    private static final ModConfigSpec.ConfigValue<Boolean> SOUND_WARN = BUILDER
+    private static final ModConfigSpec.BooleanValue SOUND_WARN = BUILDER
             .comment("Plays a sound whenever entities with a neural elytra spawn")
             .define("sound_warn", true);
 
@@ -105,15 +88,6 @@ public class Config
     @SubscribeEvent
     static void onLoad(final ModConfigEvent event)
     {
-        logDirtBlock = LOG_DIRT_BLOCK.get();
-        magicNumber = MAGIC_NUMBER.get();
-        magicNumberIntroduction = MAGIC_NUMBER_INTRODUCTION.get();
-
-        // convert the list of strings into a set of items
-        items = ITEM_STRINGS.get().stream()
-                .map(itemName -> BuiltInRegistries.ITEM.get(ResourceLocation.parse(itemName)))
-                .collect(Collectors.toSet());
-
         Optional<EntityType<?>> entityTypeOptional = EntityType.byString(SPAWNED_ENTITY_TYPE.get());
         if (entityTypeOptional.isPresent()) {
             spawnedEntityType = entityTypeOptional.get();
