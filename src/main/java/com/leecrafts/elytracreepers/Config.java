2e087f622b8166c713a95c76ebc145c70945215a
Wenhan-Samuel-Lee <wenhan_lee@utexas.edu>
2025-01-16 13:53:47 -0600

    got rid of unnecessary code

--- src/main/java/com/leecrafts/elytracreepers/Config.java
+++ src/main/java/com/leecrafts/elytracreepers/Config.java
@@ -1,18 +1,12 @@
 package com.leecrafts.elytracreepers;
 
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.config.ModConfigEvent;
 import net.neoforged.neoforge.common.ModConfigSpec;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 // An example config class. This is not required, but it's a good idea to have one to keep your config organized.
 // Demonstrates how to use Neo's config APIs
@@ -65,10 +59,6 @@ public class Config
 
     static final ModConfigSpec SPEC = BUILDER.build();
 
-    public static boolean logDirtBlock;
-    public static int magicNumber;
-    public static String magicNumberIntroduction;
-    public static Set<Item> items;
     public static EntityType<?> spawnedEntityType;
     public static boolean griefing;
     public static boolean autoIgnite;
@@ -79,11 +69,6 @@ public class Config
     public static boolean insomniaOnlySpawn;
     public static boolean subtitleWarn;
     public static boolean soundWarn;
-
-    private static boolean validateItemName(final Object obj)
-    {
-        return obj instanceof String itemName && BuiltInRegistries.ITEM.containsKey(ResourceLocation.parse(itemName));
-    }
 
     @SubscribeEvent
     static void onLoad(final ModConfigEvent event)
